034c8c63ea96233edc948297acd8ae5f7a0e3019
erteck <A01378966@itesm.mx>
2021-03-19 08:11:14 -0600

    nuevos cambios

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -10,4 +10,5 @@ print("Líneas agregadas en la rama 2")
 IGS = cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
 print(IGS)
 print(IGS.shape)
-
+#guardar imagen en escala de grises
+cv2.imwrite('004GS.jpg',IGS)
